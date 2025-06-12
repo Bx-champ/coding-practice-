291117cdef6a08464b9fd1c4ece468eb373070ce
Bx-champ <95280251+Bx-champ@users.noreply.github.com>
2025-06-12 15:28:35 +0530

    Create most_repeat.cpp
    program reads a string input and identifies the word with the most frequent repeating character among all words. It then prints the maximum frequency and the word in which that frequency occurs.

---
+++ most_repeat.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    string w="";
+    string result;
+    int max=INT_MIN;
+    cout<<"enter the string :";
+    getline(cin,s);
+    for(int i=0;i<s.size();i++){
+        if(s[i]!=' '){
+            w=w+s[i];
+        }else{
+            // cout<<"word is :"<<w;
+            map<char,int> mpp;
+            for(int j=0;j<w.size();j++){
+                mpp[s[j]]++;
+            }
+            
+            for(auto it:mpp){
+              if(it.second>max){
+                  max=it.second;
+                  result=w;
+              }  
+            }
+            w="";
+        }
+    }
+    cout<<max;
+    cout<<result;
+}
